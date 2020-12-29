b8a7376326d6d6bcca3ec6fc9b37c5184f67e86f
Tharindu Gihan <wtgihan@gmail.com>
2020-12-30 01:38:34 +0530

    Front end part #Done

---
+++ charge.php
@@ -0,0 +1,29 @@
+<?php 
+    require_once('vendor/autoload.php');
+
+    \Stripe\Stripe::setApiKey('sk_test_51HyZrDHjjnJcmL75t5HSvCcjQyGdy3759Vn6zJNWasJ5EmUJjdUIps5rcys9U7VjqS51v02i90qRqSiKd66qFEOt00YdP2Gu4R');
+
+    // Sanitize POST Array
+    $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+
+    $first_name = $POST['first_name'];
+    $last_name = $POST['last_name'];
+    $email = $POST['email'];
+    $token = $POST['stripeToken'];
+
+    // Create Customer In Stripe
+    $customer = \Stripe\Customer::create(array(
+        "email" => $email,
+        "source" => $token
+    ));
+
+    // Charge Customer
+    $charge = \Stripe\Charge::create(array(
+        "amount" => 5000,
+        "currency" => "usd",
+        "description" => "Intro To React Course",
+        "customer" => $customer->id
+    ));
+
+    // Redirect to success Page
+    header('Location: success.php?tid='.$charge->id.'&product='.$charge->description);
